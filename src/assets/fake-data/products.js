1493ffdef054d71dbc5220886c86bda81b629e1d
Venkadd <127650307+Venkadd@users.noreply.github.com>
2023-04-28 17:08:38 +0530

    Add files via upload

---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,183 @@
+// all images imported from images directory
+import product_01_image_01 from "../images/lak.jpg";
+import product_01_image_02 from "../images/lak1.jpg";
+import product_01_image_03 from "../images/lak2.jpg";
+
+import product_02_image_01 from "../images/amc1.jpg";
+import product_02_image_02 from "../images/amc2.jpg";
+import product_02_image_03 from "../images/amc3.jpg";
+
+import product_03_image_01 from "../images/cm1.jpg";
+import product_03_image_02 from "../images/cm2.jpg";
+import product_03_image_03 from "../images/cm3.jpg";
+
+import product_04_image_01 from "../images/fp1.jpg";
+import product_04_image_02 from "../images/fp2.jpg";
+import product_04_image_03 from "../images/fp3.jpg";
+
+import product_05_image_01 from "../images/gc1.jpg";
+import product_05_image_02 from "../images/gc2.jpg";
+import product_05_image_03 from "../images/gc3.jpg";
+
+import product_06_image_01 from "../images/ms1.jpg";
+import product_06_image_02 from "../images/ms2.jpg";
+import product_06_image_03 from "../images/ms1.jpg";
+
+const products = [
+  {
+    id: "01",
+    title: "Lakshmi",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "ONE SOUND CRACKERS",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+  },
+
+  {
+    id: "02",
+    title: "Ivory 6 Shots Red / Green",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "AMAZING MULTI COLOR SHOTS",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "03",
+    title: "Annam Deluxe 10 In 1",
+    price: 110.0,
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    image03: product_03_image_03,
+    category: "COLOR MATCHES (NET RATE -NO DISCOUNT)",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "04",
+    title: "Flower Pots Big",
+    price: 110.0,
+    image01: product_04_image_01,
+    image02: product_04_image_02,
+    image03: product_04_image_03,
+    category: "FLOWER POTS -PREMIUM QUALITY",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "05",
+    title: "Ground Chakkar Big (10 Pc)",
+    price: 24.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "Chakkra",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+  {
+    id: "06",
+    title: "Sweet Smart (5 Colors)",
+    price: 24.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "07",
+    title: "4'' Deluxe Hulk / Elephant (10 Pkts)",
+    price: 115.0,
+    image01: product_02_image_02,
+    image02: product_02_image_01,
+    image03: product_02_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "08",
+    title: "15 Multi Shots (long)",
+    price: 110.0,
+    image01: product_03_image_02,
+    image02: product_03_image_01,
+    image03: product_03_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "09",
+    title: "Hero Classic 10 In 1",
+    price: 110.0,
+    image01: product_04_image_02,
+    image02: product_04_image_01,
+    image03: product_04_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "10",
+    title: "Flower Pots Big",
+    price: 24.0,
+    image01: product_05_image_02,
+    image02: product_05_image_01,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "11",
+    title: " Midnight Dreams (5 Colors)",
+    price: 35.0,
+    image01: product_06_image_01,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "12",
+    title: "Sweet Smart (5 Colors)",
+    price: 35.0,
+    image01: product_06_image_02,
+    image02: product_06_image_01,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "13",
+    title: "Sweet Smart",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+];
+
+export default products;
